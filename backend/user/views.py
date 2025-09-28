91bdb1f14e7e94afe6172470afeb36726bc92310
ADMIN <thethu001@gmail.com>
2025-09-28 23:20:35 +0700

    Setup Route and Modify View API

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,17 +1,16 @@
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
-from user.serializers import LoginSerializer, RegisterSerializer
 from rest_framework import status
-from rest_framework.viewsets import ViewSet
-from .models import User
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import logout
-from rest_framework.permissions import IsAuthenticated
+from .models import User
+from user.serializers import LoginSerializer, RegisterSerializer
 # Create your views here.
 
 
 
-class LoginView(ViewSet):
+class LoginView(APIView):
     permission_classes = [AllowAny]  
 
     def post(self, request):
@@ -35,7 +34,7 @@ class LoginView(ViewSet):
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class RegisterView(ViewSet):
+class RegisterView(APIView):
     '''
     Input: username, email, firstname, lastname, and password
     Ouput: message, user infor (except password), and status
