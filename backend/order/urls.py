600ade91245077a83e6b2e9530fc66e67010b986
Thien <thiennd23416@st.uel.edu.vn>
2025-10-25 17:23:33 +0700

    "Đã xong api trễ/đúng hẹn theo top10  CategoryName"

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from order.views import SalesByPayment, SalesByMarket, SalesByRegion, LateDeliveryByRegion
+from order.views import SalesByPayment, SalesByMarket, SalesByRegion, LateDeliveryByRegion,DeliveryPerformancebyCategories
 
 urlpatterns = [
    path('stats/sales-by-payment/', SalesByPayment.as_view(), name="Sales by Payment"),
    path('stats/sales-by-market/', SalesByMarket.as_view(), name="Sales by Market"),
    path('stats/sales-by-region/', SalesByRegion.as_view(), name="Sales by Region"),
-   path('stats/latedelivery-by-region/', LateDeliveryByRegion.as_view(), name="Sales by Region")
+   path('stats/latedelivery-by-region/', LateDeliveryByRegion.as_view(), name="Sales by Region"),
+   path('stats/deliveryperformance-by-categories/', DeliveryPerformancebyCategories.as_view(), name="Delivery Performance by Top 10 Categories")
 ]
 
 
