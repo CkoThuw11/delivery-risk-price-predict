604963e879a60f59ddb894bbd73980d37ed2f2ba
ngtran2310 <trannguyen.231005@gmail.com>
2025-11-02 15:34:18 +0700

    feat: logout

--- frontend/src/components/layouts/MainLayout.jsx
+++ frontend/src/components/layouts/MainLayout.jsx
@@ -5,12 +5,23 @@ import { useState } from "react";
 const MainLayout = (props) => {
   const [activeTab, setActiveTab] = useState("statistics");
   const navigate = useNavigate();
-
+  const handleLogoutButton = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
   return (
-    <>
+    <div>
       <div className="min-h-screen bg-[#ffffff] flex flex-col p-0">
         {/* --- Header Tabs --- */}
-        <div className="flex items-center justify-center space-x-10 h-16 bg-secondary-1">
+        <div className="flex items-center justify-center space-x-10 h-16 bg-secondary-1 relative">
+          <div className="p-0 ">
+            <button
+              className="absolute top-[11px] right-[11px] border bg-primary-2 text-black pr-4 pl-4 pt-2 pb-2 rounded-xl"
+              onClick={handleLogoutButton}
+            >
+              Logout
+            </button>
+          </div>
           <TabButton
             label="Statistics"
             isActive={activeTab === "statistics"}
@@ -30,7 +41,7 @@ const MainLayout = (props) => {
         </div>
         <Outlet />
       </div>
-    </>
+    </div>
   );
 };
 
